4c870d0b589fd04f05bb924c6778938f682e6e28
chrisi.gusenbauer101@gmail.com <chrisi.gusenbauer101@gmail.com>
2023-05-08 18:02:40 +0200

    Initial version of ebayklon generated by generator-jhipster@7.9.3

---
+++ src/test/java/at/jku/IntegrationTest.java
@@ -0,0 +1,22 @@
+package at.jku;
+
+import at.jku.EbayklonApp;
+import at.jku.config.AsyncSyncConfiguration;
+import at.jku.config.EmbeddedSQL;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { EbayklonApp.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
